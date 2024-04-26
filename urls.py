5a96ccd6366a7ef4b37caa340fb8fd8ddf8ee4b3
srija0228 <147516255+srija0228@users.noreply.github.com>
2024-04-26 23:59:10 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,22 +1,15 @@
-# gallery/urls.py
-from django.urls import path
-from .views import *
-from django.contrib.auth.views import PasswordResetDoneView
+# art_gallery/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth.views import PasswordResetDoneView,LoginView, LogoutView, PasswordResetView 
+from django.conf import settings
+from django.conf.urls.static import static
+from gallery.views import *
 
 urlpatterns = [
-    path('', landing_page, name="landing_page"),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('home/', home, name='home'),
-    path('artwork/<int:pk>/', ArtworkDetailView.as_view(), name='artwork-detail'),
-    path('cart/', cart, name='cart'),
-    path('add-to-cart/<int:item_id>/', add_to_cart, name='add_to_cart'),
-    # path('remove-from-cart/<int:item_id>/', CartView.remove_from_cart(), name='remove_from_cart'),
-    path('resetpwd/', CustomPasswordResetView.as_view(), name='password_reset'),
-    path('password_reset_done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('accounts/profile/', UserProfileView.as_view(), name='user-profile'),
-    path('signup/', SignUpView.as_view(), name='signup'),
-    path('allimages/', all_images, name='all_images'),
-    path('upload/', upload_artwork, name='upload_artwork'),
+    path('admin/', admin.site.urls),
+    path('', include('gallery.urls')),
 ]
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
